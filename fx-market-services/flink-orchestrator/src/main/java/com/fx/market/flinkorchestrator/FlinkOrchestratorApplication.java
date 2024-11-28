6f53c04a1c1644877afa06d8e0e4d656b538127c
Michal Jereczek <michal.jereczek@outlook.com>
2024-11-28 22:10:24 +0100

    Flink flow for fx rates - initial

---
+++ fx-market-services/flink-orchestrator/src/main/java/com/fx/market/flinkorchestrator/FlinkOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.fx.market.flinkorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkOrchestratorApplication.class, args);
+	}
+
+}
