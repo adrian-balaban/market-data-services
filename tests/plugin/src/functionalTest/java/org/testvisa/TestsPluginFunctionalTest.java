50e8f0186ad32f1de8ac86c1e83062ba899e8860
nikitozeg <tazg@ya.ru>
2024-12-03 14:14:18 +0100

    - fixed request to /emitEvent

--- tests/plugin/src/functionalTest/java/org/testvisa/TestsPluginFunctionalTest.java
+++ tests/plugin/src/functionalTest/java/org/testvisa/TestsPluginFunctionalTest.java
@@ -1,6 +1,7 @@
 package org.testvisa;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestsPluginFunctionalTest {
@@ -25,21 +27,20 @@ class TestsPluginFunctionalTest {
 
     @Test
     void testEmitEventEndpoint() throws Exception {
-        BloombergFXRateReading rateReading = createSampleFXRateReading();
-        HttpResponse<String> response = sendPostRequest("/emitEvent", rateReading);
-        assertEquals(200, response.statusCode(), "Unexpected HTTP status code");
-    }
-
-    //temporary solution
-    private BloombergFXRateReading createSampleFXRateReading() {
-        return new BloombergFXRateReading(
-                "USD/JPY",
-                "USD",
-                "JPY",
-                110.45,
-                108.45,
-                System.currentTimeMillis()
+        BloombergFXRateReading reading1 = new BloombergFXRateReading(
+                "USD/JPY", "USD", "JPY", 110.45, 108.45, System.currentTimeMillis()
         );
+
+        BloombergFXRateReading reading2 = new BloombergFXRateReading(
+                "EUR/USD", "EUR", "USD", 1.1111, 1.1101, System.currentTimeMillis()
+        );
+
+        List<BloombergFXRateReading> readings = new ArrayList<>();
+        readings.add(reading1);
+        readings.add(reading2);
+
+        HttpResponse<String> response = sendPostRequest("/emitEvent", readings);
+        assertEquals(200, response.statusCode(), "Unexpected HTTP status code");
     }
 
     private HttpResponse<String> sendPostRequest(String endpoint, Object body) throws Exception {
