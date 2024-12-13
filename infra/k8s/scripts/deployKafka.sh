ea39c5c0c47ddba104acbcaba5c49482e52e6884
jeremiah <jeremiah@localhost.localdomain>
2024-12-13 15:24:51 +0100

    Stable deplopy all script for K8s

--- infra/k8s/scripts/deployKafka.sh
+++ infra/k8s/scripts/deployKafka.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
-separator="==================================="
-NAMESPACE=fxmarket
+trap "exit 1" TERM
+export TOP_PID=$$
+
+SEPARATOR="==================================="
+
+NAMESPACE="fxmarket" # default if not provided
+
+print_usage() {
+  echo "Usage:"
+  echo "-n <namespace>                <- to specify namespace"
+}
+
+# Parse command-line options
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    -n) NAMESPACE="$2"; shift ;;
+    *) print_usage; exit 1 ;;
+  esac
+  shift
+done
 ###############################################################
 
 kubectl get namespace ${NAMESPACE} || kubectl create namespace ${NAMESPACE}
 
 ###############################################################
-echo "$separator"
+echo "$SEPARATOR"
 echo "DEPLOYING KAFKA - START"
-echo "$separator"
+echo "$SEPARATOR"
 
 helm repo add confluentinc https://packages.confluent.io/helm
 helm repo update
@@ -17,10 +35,15 @@ helm upgrade        --install confluent-operator \
                     confluentinc/confluent-for-kubernetes \
                     --namespace ${NAMESPACE}
 
+if [[ $? != 0 ]]; then echo "ERROR | STOP" && exit; fi # check return value, exit if not 0
+
 sed -i "s/___CHANGE_ME_NAMESPACE___/${NAMESPACE}/g" ../helm/kafka/confluent-platform-singlenode.yaml ## Set proper namespace
 kubectl apply -f ../helm/kafka/confluent-platform-singlenode.yaml
+return_status_code = $? # Save to check later after sed revert
 sed -i "s/${NAMESPACE}/___CHANGE_ME_NAMESPACE___/g" ../helm/kafka/confluent-platform-singlenode.yaml ## Revert
 
-echo "$separator"
+if [[ $return_status_code != 0 ]]; then echo "ERROR | STOP" && exit; fi # check return value, exit if not 0
+
+echo "$SEPARATOR"
 echo "DEPLOYING KAFKA - END"
-echo "$separator"
+echo "$SEPARATOR"
--- infra/services/docker/flink-orchestrator/build-image.sh
+++ infra/services/docker/flink-orchestrator/build-image.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
-set -e
+set -e # exit immediately if any command within the script returns a non-zero exit status
+
+DOCKER_REGISTRY="docker.io" # default if not provided
+TAG='0.0.1' # default if not provided
+
+print_usage() {
+  echo "Usage:"
+  echo "-tag <docker_tag>             <- to specify docker tag for services"
+  echo "-registry <DOCKER_REGISTRY>   <- to specify docker registry"
+}
+
+# Parse command-line options
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    -tag) TAG="$2"; shift ;;
+    -registry) DOCKER_REGISTRY="$2"; shift ;;
+    *) print_usage; exit 1 ;;
+  esac
+  shift
+done
 
 echo "Building OSI Image - flink-orchestrator - Start"
-pushd ../../../../fx-market-services && ./gradlew flink-orchestrator:clean && ./gradlew flink-orchestrator:bootBuildImage && popd
 
+pushd ../../../../fx-market-services
 
-#docker run docker.io/library/flink-orchestrator:latest
+./gradlew flink-orchestrator:clean && \
+./gradlew flink-orchestrator:bootBuildImage -Pversion=${TAG} -Pregistry=${DOCKER_REGISTRY} \
+
+popd
 
 echo "Building OSI Image - flink-orchestrator - Finish"
