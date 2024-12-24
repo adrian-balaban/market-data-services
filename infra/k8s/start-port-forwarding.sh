22790ccac928c803b10e72ae36b18ffa2a77fb20
jeremiah <jereczekmichal@gmail.com>
2024-12-24 15:21:56 +0100

    Fix port forwarding

--- infra/k8s/start-port-forwarding.sh
+++ infra/k8s/start-port-forwarding.sh
@@ -13,7 +13,7 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 #CD
-kubectl port-forward svc/argocd-server -n ${NAMESPACE} 38080:443
+kubectl port-forward svc/argocd-server -n ${NAMESPACE} 38080:443 &
 
 #TOOLS
 kubectl port-forward -n ${NAMESPACE} svc/kafka 9092:9092 &
@@ -21,6 +21,6 @@ kubectl port-forward -n ${NAMESPACE} svc/controlcenter 9021:9021 &
 kubectl port-forward -n ${NAMESPACE} svc/fx-flink-jobmanager 8081:8081 &
 
 #STUBS
-kubectl port-forward -n ${NAMESPACE} svc/fx-market-data-stub-svc 3080:8081 &
+kubectl port-forward -n ${NAMESPACE} svc/fx-market-data-stub-svc 3080:3080 &
 
 #SERVICES (Add only if needed i.e. by e2e tests)
