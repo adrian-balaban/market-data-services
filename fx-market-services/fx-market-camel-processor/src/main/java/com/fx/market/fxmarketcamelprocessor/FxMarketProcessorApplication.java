c24ede5b3fbf81f0c9bdd09efc773b6f0969f19e
Adrian Balaban <adrian.balaban@arnia.ro>
2024-12-30 09:36:52 +0200

    Updated for finishing PR

---
+++ fx-market-services/fx-market-camel-processor/src/main/java/com/fx/market/fxmarketcamelprocessor/FxMarketProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.fx.market.fxmarketcamelprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxMarketProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxMarketProcessorApplication.class, args);
+	}
+
+}
---
+++ fx-market-services/fx-market-camel-processor/src/main/java/com/fx/market/fxmarketcamelprocessor/service/FxRate.java
@@ -0,0 +1,30 @@
+package com.fx.market.fxmarketcamelprocessor.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FxRate {
+
+    String pair;
+    String baseCurrency;
+    String quoteCurrency;
+    String ask; // keep as string to avoid conversion
+    String bid; // keep as string to avoid conversion
+
+
+    public static FxRate fromFxRateProto(com.fx.market.kafka.message.FxRateProto fxRateProto) {
+        return FxRate.builder()
+                .pair(fxRateProto.getPair())
+                .baseCurrency(fxRateProto.getBaseCurrency())
+                .quoteCurrency(fxRateProto.getQuoteCurrency())
+                .ask(fxRateProto.getAsk())
+                .bid(fxRateProto.getBid())
+                .build();
+    }
+}
