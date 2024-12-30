64742101d6e1ef8f12493933d52cfd1d53a08bff
jeremiah <jereczekmichal@gmail.com>
2024-12-30 22:24:22 +0100

    Kafka Advertised Listener as Node Port - but port forwarding doesn't work yet

--- infra/k8s/scripts/deployKafka.sh
+++ infra/k8s/scripts/deployKafka.sh
@@ -23,7 +23,33 @@ done
 ###############################################################
 
 kubectl get namespace ${NAMESPACE} || kubectl create namespace ${NAMESPACE}
+###############################################################
+echo "$SEPARATOR"
+echo "DEPLOYING NODEPORT FOR KAFKA - START"
+echo "$SEPARATOR"
 
+NEW_NODE_PORT=30000;
+current_highest_node_port=$(kubectl get services --all-namespaces | grep NodePort | awk '{print $6}' | awk -F'[:/]' '{print $2}' | sort -n | tail -n 1)
+# Check if the current_port is empty
+if [ -z "$current_highest_node_port" ]; then
+  echo "Setting NodePort to $NEW_NODE_PORT"
+else
+  # Increment the port by 1
+  NEW_NODE_PORT=$((current_highest_node_port + 1))
+  echo "Setting NodePort to $NEW_NODE_PORT"
+fi
+
+
+sed -i "s/___CHANGE_ME_NAMESPACE___/${NAMESPACE}/g" ../helm/kafka/kafkaNodePort.yaml ## Set proper namespace
+sed -i "s/___CHANGE_ME_KAFKA_NODEPORT___/${NEW_NODE_PORT}/g" ../helm/kafka/kafkaNodePort.yaml ## Set proper namespace
+kubectl apply -f ../helm/kafka/kafkaNodePort.yaml
+if [[ $? != 0 ]]; then echo "ERROR | STOP" && exit; fi # check return value, exit if not 0
+sed -i "s/${NAMESPACE}/___CHANGE_ME_NAMESPACE___/g" ../helm/kafka/kafkaNodePort.yaml ## Revert
+sed -i "s/${NEW_NODE_PORT}/___CHANGE_ME_KAFKA_NODEPORT___/g" ../helm/kafka/kafkaNodePort.yaml ## Revert
+
+echo "$SEPARATOR"
+echo "DEPLOYING NODEPORT FOR KAFKA - END"
+echo "$SEPARATOR"
 ###############################################################
 echo "$SEPARATOR"
 echo "DEPLOYING KAFKA - START"
@@ -38,9 +64,11 @@ helm upgrade        --install confluent-operator \
 if [[ $? != 0 ]]; then echo "ERROR | STOP" && exit; fi # check return value, exit if not 0
 
 sed -i "s/___CHANGE_ME_NAMESPACE___/${NAMESPACE}/g" ../helm/kafka/confluent-platform-singlenode.yaml ## Set proper namespace
+sed -i "s/___CHANGE_ME_KAFKA_NODEPORT___/${NEW_NODE_PORT}/g" ../helm/kafka/confluent-platform-singlenode.yaml ## Set proper namespace
 kubectl apply -f ../helm/kafka/confluent-platform-singlenode.yaml
 return_status_code=$? # Save to check later after sed revert
 sed -i "s/${NAMESPACE}/___CHANGE_ME_NAMESPACE___/g" ../helm/kafka/confluent-platform-singlenode.yaml ## Revert
+sed -i "s/${NEW_NODE_PORT}/___CHANGE_ME_KAFKA_NODEPORT___/g" ../helm/kafka/confluent-platform-singlenode.yaml ## Revert
 
 if [[ $return_status_code != 0 ]]; then echo "ERROR | STOP" && exit; fi # check return value, exit if not 0
 
