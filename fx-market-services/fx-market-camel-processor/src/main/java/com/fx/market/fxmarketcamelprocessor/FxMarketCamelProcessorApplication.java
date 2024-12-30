dea9abf052e67cb7f9247f5951d4699bd81e4bd5
Adrian Balaban <adrian.balaban@arnia.ro>
2024-12-30 10:12:13 +0200

    Updated for finishing PR

--- fx-market-services/fx-market-camel-processor/src/main/java/com/fx/market/fxmarketcamelprocessor/FxMarketCamelProcessorApplication.java
+++ fx-market-services/fx-market-camel-processor/src/main/java/com/fx/market/fxmarketcamelprocessor/FxMarketCamelProcessorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FxMarketProcessorApplication {
+public class FxMarketCamelProcessorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FxMarketProcessorApplication.class, args);
+		SpringApplication.run(FxMarketCamelProcessorApplication.class, args);
 	}
 
 }
