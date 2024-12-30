181fa9dcdb30d34a7a1ec9f1082a9588c98dfa38
jeremiah <jereczekmichal@gmail.com>
2024-12-30 22:24:22 +0100

    Kafka Advertised Listener as Node Port - but port forwarding doesn't work yet

--- infra/k8s/start-port-forwarding.sh
+++ infra/k8s/start-port-forwarding.sh
@@ -13,7 +13,7 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 #TOOLS
-kubectl port-forward -n ${NAMESPACE} svc/kafka 9092:9092 &
+#kubectl port-forward -n ${NAMESPACE} svc/kafka 9092:9092 &
 kubectl port-forward -n ${NAMESPACE} svc/controlcenter 9021:9021 &
 kubectl port-forward -n ${NAMESPACE} svc/fx-flink-jobmanager 8081:8081 &
 
@@ -21,3 +21,7 @@ kubectl port-forward -n ${NAMESPACE} svc/fx-flink-jobmanager 8081:8081 &
 kubectl port-forward -n ${NAMESPACE} svc/fx-market-data-stub-svc 3080:8081 &
 
 #SERVICES (Add only if needed i.e. by e2e tests)
+
+KAFKA_PORT=`kubectl get service/kafka-nodeport-service -n $NAMESPACE -o jsonpath="{..ports[0].nodePort}"`
+echo "$KAFKA_PORT"
+kubectl port-forward -n ${NAMESPACE} service/kafka-nodeport-service $KAFKA_PORT:9092 &
