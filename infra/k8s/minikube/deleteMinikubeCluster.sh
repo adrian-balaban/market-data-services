646b7a7a5ee7fc2c0419cf7afe7e8c8482eea6a4
Adrian Balaban <adrian.balaban@arnia.ro>
2025-01-03 18:32:00 +0200

    Updated for finishing PR

--- infra/k8s/minikube/deleteMinikubeCluster.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e # exit immediately if any command within the script returns a non-zero exit status
-set -o xtrace
-
-minikube delete --all=true
--- infra/k8s/scripts/buildSolution.sh
+++ infra/k8s/scripts/buildSolution.sh
@@ -32,7 +32,6 @@ sleep 2
 pushd ../../services/docker/fx-market-connector && ./build-image.sh -tag ${TAG} -registry ${DOCKER_REGISTRY} -profile k8s && popd
 pushd ../../services/docker/fx-market-camel-connector && ./build-image.sh -tag ${TAG} -registry ${DOCKER_REGISTRY} -profile k8s && popd
 pushd ../../services/docker/fx-market-processor && ./build-image.sh -tag ${TAG} -registry ${DOCKER_REGISTRY} -profile k8s && popd
-pushd ../../services/docker/fx-market-camel-processor && ./build-image.sh -tag ${TAG} -registry ${DOCKER_REGISTRY} -profile k8s && popd
 pushd ../../services/docker/flink-orchestrator && ./build-image.sh -tag ${TAG} -registry ${DOCKER_REGISTRY} -profile k8s && popd
 
 echo "$SEPARATOR"
