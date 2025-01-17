3e9efcd136822449e79148b918d22aa884a898f2
Adrian Balaban <adrian.balaban@arnia.ro>
2025-01-17 12:50:26 +0200

    polishing

--- infra/terraform/modules/kind/main.tf
+++ infra/terraform/modules/kind/main.tf
@@ -10,22 +10,3 @@ resource "kind_cluster" "kind" {
     null_resource.delete_kind_instance
   ]
 }
-
-resource "terraform_data" "install_registry" {
-  provisioner "local-exec" {
-    command = "helm install docker-registry gmelillo/docker-registry -n kube-system"
-    #command = "helm repo add gmelillo https://helm.melillo.me && helm repo update && helm install docker-registry gmelillo/docker-registry -n kube-system"
-  }
-  depends_on = [
-    kind_cluster.kind
-  ]
-}
-data "kubernetes_service" "registry_svc" {
-  metadata {
-    name      = "docker-registry"
-    namespace = "kube-system"
-  }
-  depends_on = [
-    terraform_data.install_registry
-  ]
-}
--- infra/terraform/modules/knative-camel-k-yaml/variables.tf
+++ infra/terraform/modules/knative-camel-k-yaml/variables.tf
@@ -1,6 +1,3 @@
 variable "namespace_camel_k_installation" {
   type = string
 }
-variable "registry_svc_ip" {
-  type = string
-}
