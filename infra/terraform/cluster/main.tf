f49d8c0523af0e6945e75b67fa3c14f4c2a56981
Adrian Balaban <adrian.balaban@arnia.ro>
2025-01-17 15:02:40 +0200

    How can I install knative and connector based on knative? <- Is it possible without destroying my current kind cluster?         Yes         Added terraform/README.md file that contains the info to install Knative+Camel-K demo             on a running Kind cluster             partially documented for Minikube     Can you add general terraform README, how to run terraform, what are preqrequisites etc.? Please see: infra/k8s/README.md         Done     If it is possible can you add scripts or commands in readme how to run only kind cluster on terraform, deploy only kafka, and deploy knative solution         Done, and to deploy only kafka: is not needed, for Knative+Camel-K demo

--- infra/terraform/cluster/main.tf
+++ infra/terraform/cluster/main.tf
@@ -1,15 +1,15 @@
 // for updating docs use: terraform-docs markdown . > Readme.md
 
-module "kind_cluster" {
+/*module "kind_cluster" {
   source       = "../modules/kind"
   providers    = { kind = kind, kubernetes = kubernetes.kubernetes_kind, kubectl = kubectl.kubectl_kind }
   cluster_name = var.cluster_name_kind
-}
+}*/
 module "kind_camel-k" {
   source                         = "../modules/knative-camel-k-yaml"
-  providers                      = { kubernetes = kubernetes.kubernetes_kind, kubectl = kubectl.kubectl_kind, null = null.null_kind }
+  #providers                      = { kubernetes = kubernetes.kubernetes_kind, kubectl = kubectl.kubectl_kind, null = null.null_kind }
   namespace_camel_k_installation = var.namespace_camel_k_installation
-  depends_on                     = [module.kind_cluster]
+  //depends_on                     = [module.kind_cluster]
 }
 /*module "kafka-confluent-minimal" {
   source = "../modules/kafka-confluent-minimal"
@@ -27,12 +27,6 @@ module "minikube_camel-k" {
   providers = { kubernetes = kubernetes.kubernetes_minikube,kubectl = kubectl.kubectl_minikube }
   namespace_camel_k_installation = var.namespace_camel_k_installation
   registry_svc_ip = module.minikube_cluster.registry_svc_ip #minikube_plugin_registry_svc_ip
-  depends_on = [module.minikube_cluster]
-}
-/*module "minikube_kafka-strimzi" {
-  count = var.minikube ? 1 : 0
-  source = "../modules/kafka-strimzi"
-  providers = { kubernetes = kubernetes.kubernetes_minikube,kubectl = kubectl.kubectl_minikube }
   depends_on = [module.minikube_cluster]
 }
 module "kafka-confluent-minimal" {
