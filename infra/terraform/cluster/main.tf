6c417c5e44ff2b5a2c870f8bbdcf66654b6b0b78
Adrian Balaban <adrian.balaban@arnia.ro>
2025-01-22 11:35:28 +0200

    - Done Maven registry installed, configured, and publishing of the libs, see README.md - Done Terraform installation of the 2 springboot solutions in order, dev and then test, in namespaces fxmarket and test , with building only for the dev namespace - Testing Terraform installation of Knative only once, globally, using module knative-yaml - Testing Terraform installation of the 2 camel-k solutions in order, dev and then test, in namespaces camel-k-dev and camel-k-test - Testing FxMarketConnectorSinkToKafka that uses the libs published to maven repo

--- infra/terraform/cluster/main.tf
+++ infra/terraform/cluster/main.tf
@@ -1,7 +1,7 @@
 module "springboot_solution_dev" {
   source                         = "../modules/springboot_solution"
   tag                            = var.tag
-  build = "true"
+  build = var.build
   publish_libs_to_maven_repo = "false"
   test = var.test
   namespace = var.namespaces_springboot_solution["dev"]
@@ -9,9 +9,10 @@ module "springboot_solution_dev" {
   registry_port = var.registry_port
 }
 module "springboot_solution_test" {
+  count = var.deploy_test ? 1 : 0
   source                         = "../modules/springboot_solution"
   tag                            = var.tag
-  build = var.build
+  build = false
   publish_libs_to_maven_repo = "false"
   test = var.test
   namespace = var.namespaces_springboot_solution["test"]
@@ -29,6 +30,7 @@ module "camel_k_solution_dev" {
   depends_on = [module.knative, module.springboot_solution_dev]
 }
 module "camel_k_solution_test" {
+  count = var.deploy_test ? 1 : 0
   source = "../modules/camel-k-solution"
   namespace = var.namespaces_camel_k_solution["test"]
   depends_on = [module.knative, module.springboot_solution_test]
