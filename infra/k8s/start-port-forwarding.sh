b32f3b4fab33fce97c890e9d0c4981f282c1c571
Adrian Balaban <50263077+adrian-n-balaban@users.noreply.github.com>
2025-02-13 16:26:20 +0200

    Tests impement on ci cd (#27)

--- infra/k8s/start-port-forwarding.sh
+++ infra/k8s/start-port-forwarding.sh
@@ -2,6 +2,7 @@
 
 NAMESPACE="fxmarket" # default if not provided
 
+ps -ef | grep kubectl | grep port
 
 # Parse command-line options
 while [[ "$#" -gt 0 ]]; do
@@ -21,11 +22,11 @@ echo "ARGOCD PASSWORD: ${PASSWD}"
 
 #TOOLS
 kubectl port-forward -n ${NAMESPACE} svc/kafka 9093:9092 &
-kubectl port-forward -n ${NAMESPACE} svc/controlcenter 9021:9021 &
 kubectl port-forward -n ${NAMESPACE} svc/fx-flink-jobmanager 8081:8081 &
 
 #STUBS
 kubectl port-forward -n ${NAMESPACE} svc/fx-market-data-stub-svc 3080:3080 &
+#kubectl port-forward -n ${NAMESPACE} svc/fx-market-data-stub-ws-svc 3081:3081 &
 
 #SERVICES (Add only if needed i.e. by e2e tests)
 kubectl port-forward -n ${NAMESPACE} svc/fx-market-processor-svc 4080:8080 &
