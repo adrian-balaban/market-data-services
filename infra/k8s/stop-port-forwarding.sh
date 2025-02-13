c6f84618b5a76ce3382909f53a9ef75860ad9a5c
jeremiah <jereczekmichal@gmail.com>
2025-02-13 10:23:15 +0100

    Updates after review

--- infra/k8s/stop-port-forwarding.sh
+++ infra/k8s/stop-port-forwarding.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
-ps -ef | grep kubectl | grep port
+ps -ef | grep kubectl | grep port || echo "no active port forwarding processes ."
 
-ps aux | grep -i "kubectl port-forward"  | grep -i "9093" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill
-ps aux | grep -i "kubectl port-forward"  | grep -i "8081" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill
-ps aux | grep -i "kubectl port-forward"  | grep -i "3080" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill
-ps aux | grep -i "kubectl port-forward"  | grep -i "4080" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill
-ps aux | grep -i "kubectl port-forward"  | grep -i "4081" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill
+ps aux | grep -i "kubectl port-forward"  | grep -i "9093" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill || true 
+ps aux | grep -i "kubectl port-forward"  | grep -i "8081" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill || true
+ps aux | grep -i "kubectl port-forward"  | grep -i "8081" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill || true
+ps aux | grep -i "kubectl port-forward"  | grep -i "3080" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill || true
+ps aux | grep -i "kubectl port-forward"  | grep -i "4080" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill || true
+ps aux | grep -i "kubectl port-forward"  | grep -i "4081" | grep -v grep | awk {'print $2'} | xargs --no-run-if-empty kill || true
+
+echo "Port forwarrding stopped"
