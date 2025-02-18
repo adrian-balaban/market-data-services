7632f3059421d3f870dec657ddea987c9087d59b
Adrian Balaban <adrian.balaban@arnia.ro>
2025-02-18 12:22:26 +0200

    deleteAll.sh taken from master

--- infra/k8s/destroyAll.sh
+++ infra/k8s/destroyAll.sh
@@ -49,8 +49,9 @@ kubectl patch kafka.platform.confluent.io/kafka -p '{"metadata":{"finalizers":[]
 timeout 5 kubectl -n ${NAMESPACE} delete zookeeper.platform.confluent.io/zookeeper --force --grace-period=0 
 timeout 5 kubectl -n ${NAMESPACE} delete kafka.platform.confluent.io/kafka --force --grace-period=0 
 
-timeout 5 kubectl delete all --all -n ${NAMESPACE}
+kubectl patch zookeeper.platform.confluent.io/zookeeper -p '{"metadata":{"finalizers":[]}}' --type=merge
 
+timeout 5 kubectl delete all --all -n ${NAMESPACE}
 
 echo "DELETING NAMESPACE"
 timeout 15 kubectl delete namespace ${NAMESPACE}
